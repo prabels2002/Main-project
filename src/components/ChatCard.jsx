97d5923c5f8a15a438f99bbd5b0f2b692a4da16c
prabel <prabels15@gmail.com>
2025-03-02 00:04:51 +0530

    home page

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -5,9 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // This is a function which renders the friends in the friends list
 export function ChatCard( props ){
     return (
-        <Row style={{ marginRight:"2px" }}>
+        <Row style={{ marginRight:"9px" }}>
             <Card border="success" style={{ width:'100%', alignSelf:'center', marginLeft:"15px" }} onClick={ () => { props.getMessages( props.publicKey ); }}>
               <Card.Body>
+                  <Card.Title> { props.name } </Card.Title>
                   <Card.Title> { props.name } </Card.Title>
                   <Card.Subtitle> { props.publicKey.length > 20 ? props.publicKey.substring(0, 20) + " ..." : props.publicKey } </Card.Subtitle>
               </Card.Body>
